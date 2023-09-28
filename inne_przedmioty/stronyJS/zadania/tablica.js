025097937e8f17826e096c2c67af98a0a66ef928
NEDRI <jedrek.iwaszkiewicz@gmail.com>
2023-09-28 14:37:04 +0200

    inne_przedmioty

---
+++ inne_przedmioty/stronyJS/zadania/tablica.js
@@ -0,0 +1,13 @@
+tb1=[5,4,2,6]
+for (let i=0; i < tb1.length; i++) {
+    cos = tb1[i]
+    console.log(cos)
+    if (i == 1){
+        console.log("\n")
+    }
+}
+
+console.log("przyklad 2\n")
+for (kaczka of tb1){
+    console.log(kaczka)
+}
