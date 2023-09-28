63b164bc335afa577c8959bbbd25f7ccbb3199f1
NEDRI <jedrek.iwaszkiewicz@gmail.com>
2023-09-28 15:04:22 +0200

    a

---
+++ inne_przedmioty/stronyJS/mein/script.js
@@ -0,0 +1,21 @@
+//zad1
+let liczba = 5
+for (let i=0; i <= 10; i++) {
+    console.log(liczba)
+    liczba = liczba + 1
+}
+
+//zad2
+//let a = prompt("podaj a: ")
+//let b = prompt("podaj b: ")
+//console.log(a,b)
+
+//zad3
+for (let q=0; q < 5; q++) {
+    console.log("andrzej")
+}
+
+//zad4
+for(let w=0; w <= 10; w++){
+    console.log(w + "linia")
+}
