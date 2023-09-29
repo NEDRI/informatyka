95e597e4a7880ec0d56d967acfba55edd42890b0
NEDRI <jedrek.iwaszkiewicz@gmail.com>
2023-09-29 11:08:27 +0200

    koniec_zad_js

--- inne_przedmioty/stronyJS/mein/script.js
+++ inne_przedmioty/stronyJS/mein/script.js
@@ -21,3 +21,44 @@ for(let w=0; w <= 10; w++){
 }
 
 //zad5
+let wysokosc = 5
+let szerokosc = 10
+for (let bok10 = 0; bok10 < wysokosc; bok10++) {
+    let wiersz = '';
+    for (let bok5 = 0; bok5 < szerokosc; bok5++) {
+      wiersz += '*';
+    }
+    console.log(wiersz);
+  }
+
+//zad6
+/*
+let wysokosc6 = prompt("podaj wyskosc: ")
+let szerokosc6 = prompt("podaj szerokosc: ")
+for (let bok106 = 0; bok106 < wysokosc6; bok106++) {
+    let wiersz6 = '';
+    for (let bok56 = 0; bok56 < szerokosc6; bok56++) {
+      wiersz6 += '*';
+    }
+    console.log(wiersz6);
+  }
+*/
+
+//zad7
+for (let m = 1; m <= 10; m++) {
+    for (let n = 1; n <= 10; n++) {
+      const wynik = m * n;
+      console.log(`${m} * ${n} = ${wynik}`);
+    }
+  }
+
+//zad8  
+let wysokoscTrojkata = 5;
+
+for (let p = 1; p <= wysokoscTrojkata; p++) {
+  let gwiazdki = '';
+  for (let o = 1; o <=p; o++) {
+    gwiazdki += '*';
+  }
+  console.log(gwiazdki);
+}
