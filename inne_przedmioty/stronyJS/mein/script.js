9f7f080e827957b1add758122bbce7817f678b71
NEDRI <jedrek.iwaszkiewicz@gmail.com>
2023-09-29 11:39:46 +0200

    Koniec_zad_js

--- inne_przedmioty/stronyJS/mein/script.js
+++ inne_przedmioty/stronyJS/mein/script.js
@@ -1,4 +1,5 @@
 //zad1
+console.log("1")
 let liczba = 5
 for (let i=0; i <= 10; i++) {
     console.log(liczba)
@@ -6,6 +7,8 @@ for (let i=0; i <= 10; i++) {
 }
 
 //zad2
+console.log("")
+console.log("2")
 let z= 5
 let x=10
 for(let c=z; c <=x; c++){
@@ -14,16 +17,22 @@ for(let c=z; c <=x; c++){
 
 
 //zad3
+console.log("")
+console.log("3")
 for (let q=0; q < 5; q++) {
     console.log("andrzej")
 }
 
 //zad4
+console.log("")
+console.log("4")
 for(let w=0; w <= 10; w++){
     console.log(w + "linia")
 }
 
 //zad5
+console.log("")
+console.log("5")
 let wysokosc = 5
 let szerokosc = 10
 for (let bok10 = 0; bok10 < wysokosc; bok10++) {
@@ -36,6 +45,8 @@ for (let bok10 = 0; bok10 < wysokosc; bok10++) {
 
 //zad6
 /*
+console.log("")
+console.log("6")
 let wysokosc6 = prompt("podaj wyskosc: ")
 let szerokosc6 = prompt("podaj szerokosc: ")
 for (let bok106 = 0; bok106 < wysokosc6; bok106++) {
@@ -48,6 +59,8 @@ for (let bok106 = 0; bok106 < wysokosc6; bok106++) {
 */
 
 //zad7
+console.log("")
+console.log("7")
 for (let m = 1; m <= 10; m++) {
     for (let n = 1; n <= 10; n++) {
       const wynik = m * n;
@@ -56,6 +69,8 @@ for (let m = 1; m <= 10; m++) {
   }
 
 //zad8
+console.log("")
+console.log("8")
 let wysokoscTrojkata = 5;
 
 for (let p = 1; p <= wysokoscTrojkata; p++) {
