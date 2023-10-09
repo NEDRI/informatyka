3f4c5ba31ea0c1546cc432eb9d3b6f80fb82402f
NEDRI <jedrek.iwaszkiewicz@gmail.com>
2023-10-09 11:08:50 +0200

    Updated <file_name>

--- inf/Funkcja_szybkiego_potęgowania_metodą_iteracyjną.py
+++ inf/Funkcja_szybkiego_potęgowania_metodą_iteracyjną.py
@@ -1,6 +1,6 @@
 #https://github.com/PiskorowskiJakub/programming-course-cpp/blob/main/2-algorytmika/2-2-podstawowe-algorytmy/2-2-05-potegowanie-szybkie/README.md
 
-def potegowanie_szybkie(a, n):
+def szybkie_potegowanie(a, n):
     w = 1
     while n > 0:
         if n % 2 == 1:
@@ -12,7 +12,6 @@ def potegowanie_szybkie(a, n):
 a = int(input("Podaj podstawę: "))
 n = int(input("Podaj wykładnik: "))
 
-wynik = potegowanie_szybkie(a, n)
-
+wynik = szybkie_potegowanie(a, n)
 print(f"{a} do potęgi {n} wynosi: {wynik}")
 
