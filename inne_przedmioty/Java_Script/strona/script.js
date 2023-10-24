9119bc9ca8422d419f555a89fdbbc2f3fd430420
NEDRI <jedrek.iwaszkiewicz@gmail.com>
2023-10-24 21:18:52 +0200

    22

--- inne_przedmioty/Java_Script/strona/script.js
+++ inne_przedmioty/Java_Script/strona/script.js
@@ -14,8 +14,9 @@ for (let i = 0; i < 50; i++) {
   liczby.push(Math.floor(Math.random() * 100) + 1);
 }
 
-//zad1
 /*
+//zad1
+
 let podzielnePrzez3 = 0;
 for (let i = 0; i < liczby.length; i++) {
   if (liczby[i] % 3 === 0) {
@@ -23,19 +24,18 @@ for (let i = 0; i < liczby.length; i++) {
   }
 }
 document.write("Liczb podzielnych przez 3 jest " + podzielnePrzez3);
-*/
 
 //zad2
-/*
+
 for (let i = 0; i < liczby.length; i++) {
     if (i % 2 === 0) {
       document.write("Liczba o parzystym indeksie: " + liczby[i] + "<br>");
     }
   }
-*/
+
 
 //zad3
-/*
+
 let wiekszeOd15 = 0;
 for (let i = 0; i < liczby.length; i++) {
   if (liczby[i] > 15) {
@@ -43,10 +43,10 @@ for (let i = 0; i < liczby.length; i++) {
   }
 }
 document.write("Liczb większych od 15 jest " + wiekszeOd15);
-*/
+
 
 //zad4
-/*
+
 let najmniejszaLiczba = liczby[0];
 let indeksNajmniejszej = 0;
 for (let i = 1; i < liczby.length; i++) {
@@ -56,22 +56,20 @@ for (let i = 1; i < liczby.length; i++) {
   }
 }
 document.write("Najmniejsza liczba to " + najmniejszaLiczba + " o indeksie " + indeksNajmniejszej);
-*/
+
 
 //zad5
-/*
+
 let dwucyfroweLiczby = 0;
 for (let i = 0; i < liczby.length; i++) {
   if (liczby[i] >= 10 && liczby[i] <= 99) {
     dwucyfroweLiczby++;
   }
 }
-
 document.write("Liczb dwucyfrowych jest " + dwucyfroweLiczby);
-*/
 
 //zad6
-/*
+
 function sumaCyfr(liczba) {
     let suma = 0;
     while (liczba > 0) {
@@ -80,20 +78,18 @@ function sumaCyfr(liczba) {
     }
     return suma;
   }
-  
   let parzystaSumaCyfr = 0;
   for (let i = 0; i < liczby.length; i++) {
     if (sumaCyfr(liczby[i]) % 2 === 0) {
       parzystaSumaCyfr++;
     }
   }
-  
   document.write("Liczb z parzystą sumą cyfr jest " + parzystaSumaCyfr);
-  */
-
-//zad7
-/*
-function wszystkieCyfryParzyste(liczba) {
+  
+  
+  //zad7
+  
+  function wszystkieCyfryParzyste(liczba) {
     while (liczba > 0) {
       if ((liczba % 10) % 2 !== 0) {
         return false;
@@ -102,7 +98,6 @@ function wszystkieCyfryParzyste(liczba) {
     }
     return true;
   }
-  
   for (let i = 0; i < liczby.length; i++) {
     if (wszystkieCyfryParzyste(liczby[i])) {
       document.write("Liczba, której wszystkie cyfry są parzyste: " + liczby[i] + "<br>");
