e6a5c821c51d4080b0da19c67c0a89203618c6a8
NEDRI <jedrek.iwaszkiewicz@gmail.com>
2023-10-24 21:24:10 +0200

    22

--- inf/Algorytm_rozkladu_liczb_na_czynniki_pierwsze.py
+++ inf/Algorytm_rozkladu_liczb_na_czynniki_pierwsze.py
@@ -1,11 +1,1 @@
 #https://github.com/PiskorowskiJakub/programming-course-cpp/blob/main/2-algorytmika/2-7-algorytmy-iteracyjne/2-7-01-rozklad-liczb/README.md
-def rozklad(n):
-    k = 2
-    czyniki = []
-    while n > 1:
-        while n & k == 0:
-            czyniki.append(k)
-            n = n / k
-        k = k + 1
-    print(czyniki)
-n = int(input("Podaj liczbe: "))
