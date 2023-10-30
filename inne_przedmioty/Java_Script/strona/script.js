d2fddaac5bc7a6ad6127bd779f8f5397583886cb
NEDRI <jedrek.iwaszkiewicz@gmail.com>
2023-10-30 10:07:07 +0100

    dokonczyc js

--- inne_przedmioty/Java_Script/strona/script.js
+++ inne_przedmioty/Java_Script/strona/script.js
@@ -1,3 +1,4 @@
+//zad1
 function obliczPoleIObwod() {
   var promien = parseFloat(document.getElementById("promien").value);
 
@@ -10,7 +11,15 @@ function obliczPoleIObwod() {
       document.getElementById("wynik").innerHTML = "zly promien.";
   }
 }
+
+//zad2
 tablica1 = []
 tablica1.push(Math.floor(Math.random() * 11) + 2);
 document.write("losowa liczba od 2 do 12 to: " + tablica1)
+
+//zad3
+tablica2= []
+while(i=0,i<=25,i++){
+  tablica1.push(Math.floor(Math.random() * 1) + 2);
+}
 
