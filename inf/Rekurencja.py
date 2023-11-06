4d48c4d760f6f927ced21f9cf1f7a2cd397325e0
NEDRI <jedrek.iwaszkiewicz@gmail.com>
2023-11-06 17:30:03 +0100

    a

---
+++ inf/Rekurencja.py
@@ -0,0 +1,1 @@
+#https://github.com/PiskorowskiJakub/programming-course-cpp/blob/main/1-programowanie-strukturalne/1-3-struktury-danych/1-3-7-rekurencja/README.md
--- inf/test.py
+++
@@ -1,6 +0,0 @@
-def suma_rekurencja(liczba):
-    if liczba == 0: return 0
-    return liczba + suma_rekurencja(liczba - 1)
-
-l = 4
-print(suma_rekurencja(l))
