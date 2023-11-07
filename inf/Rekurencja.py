f972ba5df23576bead84d941b10710e634d523b6
NEDRI <jedrek.iwaszkiewicz@gmail.com>
2023-11-07 09:59:34 +0100

    koniec

--- inf/Rekurencja.py
+++ inf/Rekurencja.py
@@ -1,1 +1,11 @@
 #https://github.com/PiskorowskiJakub/programming-course-cpp/blob/main/1-programowanie-strukturalne/1-3-struktury-danych/1-3-7-rekurencja/README.md
+
+def funkcja(n):
+    if n == 0:
+        return 1
+    else:
+        return n * funkcja(n - 1)
+
+n = int(input("podaj liczbe: "))
+wynik = funkcja(n)
+print(f"{n}! = {wynik}")
