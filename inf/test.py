5a722494f7fffbe31fdaa01dd7370767489aca7c
NEDRI <jedrek.iwaszkiewicz@gmail.com>
2023-11-07 11:06:19 +0100

    aaa

---
+++ inf/test.py
@@ -0,0 +1,7 @@
+#https://github.com/PiskorowskiJakub/programming-course-cpp/blob/main/2-algorytmika/2-7-algorytmy-iteracyjne/2-7-03-dec-to-bin/README.md
+
+def funkcja(n):
+    
+
+n = int(input("podja liczbe dziesietna: "))
+funkcja(n)
