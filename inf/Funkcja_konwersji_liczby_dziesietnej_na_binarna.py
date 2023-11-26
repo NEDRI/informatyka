238109d9c34c8ebc612ac853d60257879d056e1d
NEDRI <jedrek.iwaszkiewicz@gmail.com>
2023-11-26 17:40:44 +0100

    koniec

--- inf/Funkcja_konwersji_liczby_dziesietnej_na_binarna.py
+++ inf/Funkcja_konwersji_liczby_dziesietnej_na_binarna.py
@@ -1,10 +1,16 @@
 #https://github.com/PiskorowskiJakub/programming-course-cpp/blob/main/2-algorytmika/2-7-algorytmy-iteracyjne/2-7-03-dec-to-bin/README.md
-def przliczanie(a):
-    tab = []
-    while a != 0:
-        tab.append(a % 2)
-        a = a // 2
-    return tab
+def funkcja(liczba):
+    tab = [0] * 31  
+    i = 0 
+
+    while liczba != 0:
+        tab[i] = liczba % 2 
+        i += 1
+        liczba = liczba // 2  
+
+    for j in range(i - 1, -1, -1):
+        print(tab[j]) 
+
 liczba = int(input("Podaj liczbe: "))
-wynik = przliczanie(liczba)
-print(f"Liczba {liczba}  po zamianie: {wynik}")
+print(f"{liczba} to: ")
+funkcja(liczba)
