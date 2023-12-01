222828308eb8402d4cfefae4d91aa7f6507200f7
NEDRI <jedrek.iwaszkiewicz@gmail.com>
2023-12-01 11:33:41 +0100

    aa

--- inne_przedmioty/Java_Script/egzaminbezskrypt/jsnb.js
+++ inne_przedmioty/Java_Script/egzaminbezskrypt/jsnb.js
@@ -23,5 +23,38 @@ function braki(){
         p1 = document.getElementById("p1")
         p1.style.backgroundColor="Honeydew"   
     }
+
+    if (p2v === 0) {
+        p2 = document.getElementById("p2")
+        p2.style.backgroundColor="red"
+    } else if (p2v >= 1 && p2v <= 5){
+        p2 = document.getElementById("p2")
+        p2.style.backgroundColor="yellow"
+    } else {
+        p2 = document.getElementById("p2")
+        p2.style.backgroundColor="Honeydew"   
+    }
+
+    if (p3v === 0) {
+        p3 = document.getElementById("p3")
+        p3.style.backgroundColor="red"
+    } else if (p3v >= 1 && p3v <= 5){
+        p3 = document.getElementById("p3")
+        p3.style.backgroundColor="yellow"
+    } else {
+        p3 = document.getElementById("p3")
+        p3.style.backgroundColor="Honeydew"   
+    }
+    if (p4v === 0) {
+        p4 = document.getElementById("p4")
+        p4.style.backgroundColor="red"
+    } else if (p4v >= 1 && p4v <= 5){
+        p4 = document.getElementById("p4")
+        p4.style.backgroundColor="yellow"
+    } else {
+        p1 = document.getElementById("p1")
+        p1.style.backgroundColor="Honeydew"   
+    }
+
     
 }
