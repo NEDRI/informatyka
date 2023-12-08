b5d9a6347885f69261389e4f617072495b9cc1d5
NEDRI <jedrek.iwaszkiewicz@gmail.com>
2023-12-08 11:55:58 +0100

    przekobinowane

--- inne_przedmioty/Java_Script/zadprzedspr/script.js
+++ inne_przedmioty/Java_Script/zadprzedspr/script.js
@@ -1,19 +1,35 @@
+c=1
 function przel(a){
     obraz = document.getElementById("glowne")
+    if(a <= 5 ){
+        c = a
+    }
+    if (a == 9){
+        c=c+1
+    } else if (a == 8){
+        c=c-1
+    }
     if (a == 1){
-        obraz.src = '1.jpg';
-    } else if (a == 2){
+        obraz.src = '1.jpg'
+    } else if (a == 2 || c == 2){
         obraz.src = '2.jpg';
-    } else if (a == 3){
+    } else if (a == 3 || c == 3){
         obraz.src = '3.jpg';
-    } else if (a == 4){
+    } else if (a == 4 || c == 4){
         obraz.src = '4.jpg';
     } else if (a == 5){
         obraz.src = '5.jpg';
     }
-}
-function prz(){
-    obraz = document.getElementById("glowne")
-    obrazv = document.getElementById("glowne").value
-    console.log(obrazv)
+    if (c == 0){
+        c = 5
+    }else if (c == 6){
+        c = 1
+    }
+    console.log(c)
+    if(c == 1){
+        obraz.src = '1.jpg'
+    }
+    if(c == 5){
+        obraz.src = '5.jpg';
+    }
 }
