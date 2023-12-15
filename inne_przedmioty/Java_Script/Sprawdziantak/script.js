607aed9f2d8d071c5b5fd939a02e13bc1d78a591
NEDRI <jedrek.iwaszkiewicz@gmail.com>
2023-12-15 10:59:42 +0100

    1.2

--- inne_przedmioty/Java_Script/Sprawdziantak/script.js
+++ inne_przedmioty/Java_Script/Sprawdziantak/script.js
@@ -1,11 +1,21 @@
 function arytmetyczny(){
-    pierwsza = parseFloat(document.getElementById('arytmetycznypier').value);
-    ruz = parseFloat(document.getElementById('arytmetycznyruz').value);
+    pierwsza = (document.getElementById('arytmetycznypier').value);
+    ruz = (document.getElementById('arytmetycznyruz').value);
     
-    var tab = [];
-    for (var i = 0; i < 10; i++) {
+    tab = [];
+    for (i = 0; i < 5; i++) {
         tab.push(pierwsza + i * ruz);
     }
     
     document.getElementById('arytmetycznyOUT').innerHTML = tab.join(', ');
+}
+function geometryczny(){
+    var geometrycznypier = (document.getElementById('geometrycznypier').value);
+    var pierwszailoraz = (document.getElementById('pierwszailoraz').value);
+
+    tab = [];
+    for (var i = 0; i < 5; i++) {
+        tab.push(geometrycznypier * Math.pow(pierwszailoraz, i));
+    }
+    document.getElementById('geometrycznyOUT').innerText = "Wyniki: " + tab.join(', ');
 }
