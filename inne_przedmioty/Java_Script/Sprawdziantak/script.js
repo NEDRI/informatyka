e7e5aa917160bb889add82ea1644e5721f73a9d4
NEDRI <jedrek.iwaszkiewicz@gmail.com>
2023-12-15 11:48:03 +0100

    2 prawie

--- inne_przedmioty/Java_Script/Sprawdziantak/script.js
+++ inne_przedmioty/Java_Script/Sprawdziantak/script.js
@@ -25,3 +25,31 @@ function wyswietljezyk(){
     jezyk =document.getElementById("lista").value
     alert("ulubiony jezyk to: "+jezyk)
 }
+//zad2
+function funkcja2(){
+    l1 = document.getElementById("l1");
+    l2 = document.getElementById("l2");
+    l3 = document.getElementById("l3");
+    l4 = document.getElementById("l4");
+    if (l1.checked) {liczba2 = l1};
+    if (l2.checked) {liczba2 = l2};
+    if (l3.checked) {liczba2 = l3};
+    if (l4.checked) {liczba2 = l4}
+    wynik = Math.pow(parseInt(liczba2.value), 2);
+    document.getElementById("wynik2").innerHTML=wynik
+
+    c1 = document.getElementById("color1");
+    c2 = document.getElementById("color2");
+    c3 = document.getElementById("color3");
+    c4 = document.getElementById("color4");
+
+    if (c1.checked) {kolor2 = c1};
+    if (c2.checked) {kolor2 = c2};
+    if (c3.checked) {kolor2 = c3};
+    if (c4.checked) {kolor2 = c4};
+    document.getElementById("a").style.backgroundColor=kolor2
+
+  }
+      
+  
+
