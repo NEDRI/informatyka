8cb8b8f8707c7672c1a2f6a2e68596b636977f95
NEDRI <jedrek.iwaszkiewicz@gmail.com>
2023-12-23 17:52:06 +0100

    tak

--- inf/test.py
+++
@@ -1,9 +0,0 @@
-def przliczanie(a):
-    tab = [0]
-    while a != 0:
-        tab.append(a % 2)
-        a = a // 2
-    return tab
-liczba = int(input("Podaj liczbe dziesietna: "))
-wynik = przliczanie(liczba)
-print(f"Liczba {liczba}  po zamianie na postac binarna: {wynik}")
