5428fca316d343d679ba7d87d763f5975382fb2e
NEDRI <jedrek.iwaszkiewicz@gmail.ocm>
2024-04-15 14:05:07 +0200

    zadanie 2 do zrobienia jescze

---
+++ inf/Ciag_Fibonacciego.py
@@ -0,0 +1,17 @@
+#https://github.com/PiskorowskiJakub/programming-course-cpp/blob/main/2-algorytmika/2-8-algorytmy-rekurencyjne/2-8-02-ciag-fibonacciego/README.md
+def zad1():
+    def zad1(n, tab={}):
+        if n < 3:
+            return 1
+        if n in tab:
+            return tab[n]
+        else:
+            tab[n] = zad1(n - 2, tab) + zad1(n - 1, tab)
+            return tab[n]
+    #n = int(input("Podaj nr wyrazu ciagu: "))
+    n = 50
+    print(f"Wartość {n}-tego wyrazu ciągu Fibonacciego wynosi: {zad1(n)}\n")
+def zad2():
+    print("kaczka")
+
+zad1()
