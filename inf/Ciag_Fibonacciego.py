f21731590a8002286695387c2ef1d8f009494b8d
NEDRI <jedrek.iwaszkiewicz@gmail.ocm>
2024-04-16 13:15:15 +0200

    zrobione

--- inf/Ciag_Fibonacciego.py
+++ inf/Ciag_Fibonacciego.py
@@ -10,10 +10,24 @@ def zad1():
             return tab[n]
     #n = int(input("Podaj nr wyrazu ciagu: "))
     n = 50
-    print(f"Wartość {n}-tego wyrazu ciągu Fibonacciego wynosi: {zad1(n)}\n")
+    print(f"Wartość {n} tego wyrazu ciągu Fibonacciego wynosi: {zad1(n)}\n")
+
 def zad2():
     def zad2(n):
-        print("kaczka")
-    
+        f0 = 0
+        f1 = 1
+        f = 0
+        for i in range(n):
+            if i >= 1:
+                f = f0 + f1
+                f0 = f1
+                f1 = f  
+            elif f == i:
+                pass
+            print(f"{f}")
+    #n = int(input("Ile wyrazow ciagu wypisac: "))
+    n = 10
+    zad2(n)
+
 zad1()
 zad2()
