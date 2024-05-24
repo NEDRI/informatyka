8263ef9f7bcf9f62e186bf6455f8346534c39e85
NEDRI <jedrek.iwaszkiewicz@gmail.com>
2024-05-24 10:06:34 +0200

    tak

---
+++ inf/Wydawanie_reszty.py
@@ -0,0 +1,12 @@
+#https://github.com/PiskorowskiJakub/programming-course-cpp/blob/main/2-algorytmika/2-9-inne-algorytmy/2-9-02-wydawanie-reszty/README.md
+tab = [1,2,5]
+
+def przeliczanie_reszty(n,tab):
+    i = 0
+    wynik = 0
+    while n > 0:
+        
+
+
+
+n = 13
