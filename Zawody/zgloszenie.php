70cd5bac91d49167d0c91a9773bce898e8c31fb6
NEDRI <jedrek.iwaszkiewicz@gmail.com>
2024-06-06 10:55:26 +0200

    zrobione

---
+++ Zawody/zgloszenie.php
@@ -0,0 +1,27 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "wedkarstwo";
+
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+$lowisko = $_POST['lowisko'];
+$data = $_POST['data'];
+$sedzia = $_POST['sedzia'];
+
+$sql = "INSERT INTO zawody_wedkarskie (Karty_wedkarskie_id, Lowisko_id, data_zawodow, sedzia)
+VALUES (0, '$lowisko', '$data', '$sedzia')";
+
+if (mysqli_query($conn, $sql)) {
+    echo "Nowy rekord został dodany pomyślnie";
+} else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+
+mysqli_close($conn);
+?>
