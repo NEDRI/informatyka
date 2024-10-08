da2284cc95afd578d221105a9a6a09a016fd1f0f
NEDRI <jedrek.iwaszkiewicz@gmail.com>
2024-10-08 10:21:49 +0200

    kaczka

---
+++ js/kolorki/script.js
@@ -0,0 +1,9 @@
+function nie(){
+    document.getElementById("txt").style.color = "blue"    
+}
+function zie(){
+    document.getElementById("txt").style.color = "green"    
+}
+function cze(){
+    document.getElementById("txt").style.color = "red"    
+}
