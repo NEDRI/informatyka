5e3261e57f4b353d8519772fb55d054b5b786564
NEDRI <jedrek.iwaszkiewicz@gmail.com>
2024-10-17 22:42:22 +0200

    kaczka

--- js/FajneZadanko/script.js
+++ js/FajneZadanko/script.js
@@ -1,72 +1,73 @@
-
-function kolortla(){
-    r=document.getElementById("r").value
-    g=document.getElementById("b").value
-    b=document.getElementById("b").value
+function kolortla() {
+    const r = document.getElementById("r").value;
+    const g = document.getElementById("g").value;
+    const b = document.getElementById("b").value;
     document.getElementById('d2').style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
-
 }
 
 function wielkosc() {
-    w = document.getElementById("w").value;
-    h = document.getElementById("h").value;
-    obraz = document.getElementById("obraz");
-    
+    const w = document.getElementById("w").value;
+    const h = document.getElementById("h").value;
+    const obraz = document.getElementById("obraz");
+
     obraz.style.width = w + "px";
     obraz.style.height = h + "px";
 }
 
-function nie(){
-    document.getElementById("txt").style.color = "blue"    
-}
-function zie(){
-    document.getElementById("txt").style.color = "green"    
-}
-function cze(){
-    document.getElementById("txt").style.color = "red"    
+function nie() {
+    document.getElementById("txt").style.color = "blue";
 }
 
-function rozmiar(){
-    rozmiar = document.getElementById("rozmiar").value
+function zie() {
+    document.getElementById("txt").style.color = "green";
+}
+
+function cze() {
+    document.getElementById("txt").style.color = "red";
+}
+
+function rozmiar() {
+    const rozmiar = document.getElementById("rozmiar").value;
     document.getElementById("txt").style.fontSize = rozmiar + "px";
 }
 
-function txt(typ){
-    strong = document.getElementById("strong");
-    subtle = document.getElementById("subtle");
-    txt = document.getElementById("txt")
-
-    if (strong.checked) {txt.document.strong};
-    if (subtle.checked) {txt.document.su};
+function reset(txt) {
+    txt.style.fontWeight = 'normal';
+    txt.style.fontStyle = 'normal';
+    txt.style.textDecoration = 'none';
 }
 
+function st() {
+    const txt = document.getElementById("txt");
+    reset(txt);
 
-function st(){
-    strong = document.getElementById("strong");
-    txt = document.getElementById("txt");
-    if (strong.checked){
+    const strong = document.getElementById("strong");
+    if (strong.checked) {
         txt.style.fontWeight = 'bold';
-    } else {
-        txt.style.fontWeight = 'normal';  
     }
 }
 
-function su(){
-    subtle = document.getElementById("subtle");
-    txt = document.getElementById("txt");
-    if (subtle.checked){
-        txt.style.fontWeight = 'lighter'; 
-    } else {
-        txt.style.fontWeight = 'normal';
+function it() {
+    const txt = document.getElementById("txt");
+    reset(txt);
+
+    const italic = document.getElementById("italic");
+    if (italic.checked) {
+        txt.style.fontStyle = 'italic';
     }
 }
 
-function podk(){
-    podk = document.getElementById("podk");
-    txt = document.getElementById("txt");
-    if (subtle.checked){
-        txt.style.textDecoration = 'underline'; 
-    } else {
-        txt.style.textDecoration = 'none';
+function podk() {
+    const txt = document.getElementById("txt");
+    reset(txt);
+
+    const podk = document.getElementById("podk");
+    if (podk.checked) {
+        txt.style.textDecoration = 'underline';
     }
+}
+
+function liststyle(style) {
+    const list = document.getElementById("tablica");
+    list.style.listStyleType = style;
 }
