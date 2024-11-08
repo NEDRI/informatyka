a99d1d542aed4e6d20de225f112f1ea9169ec772
NEDRI <jedrek.iwaszkiewicz@gmail.com>
2024-11-08 11:02:41 +0100

    egzamin

---
+++ htmlicss/egzamin/wedkowanie.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Klub wędkowania</title>
+    <script src="script.js"></script>
+    <link rel="stylesheet" href="styl2.css">
+</head>
+<body>
+    <header>
+        <h2>Wędkuj z nami!</h2>
+    </header>
+    <section id="srodek">
+        <div id="lewo">
+            <img id="obraz" src="ryba2.jpg" alt="Szczupak">
+        </div>
+        <div id="prawo">
+            <h3>Ryby spokojnego żeru (białe)</h3>
+            <p>Tutaj skrypt</p>
+            <ol>
+                <li><a href="https://wedkuje.pl/" target="blank">Odwiedź także</a></li>
+                <li><a href="http://www.pzw.org.pl/" target="blank">Polski Związek Wędkarski</a></li>
+            </ol>
+        </div>
+    </section>
+    <footer>
+        <p>Strone wykonał: PESEL</p>
+    </footer>
+</body>
+</html>
